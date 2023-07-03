f0c33e3782042ba06f28cd9a2f64970106f963d1
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-04 08:50:45 +0900

    직접 임베드 하여 관계를 짓기

--- Models/user.js
+++ Models/user.js
@@ -7,3 +7,53 @@ mongoose
     console.log("Oh No Mongo Connection Error!");
     console.log(err);
   });
+
+const userSchema = new mongoose.Schema({
+  first: String,
+  last: String,
+  addresses: [
+    {
+      _id: { _id: false },
+      street: String,
+      city: String,
+      state: String,
+      country: String,
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const makeUser = async () => {
+  const user = new User({
+    first: "Harry",
+    last: "Potter",
+  });
+
+  user.addresses.push({
+    street: "123 Sesame St.",
+    city: "New York",
+    state: "NY",
+    country: "USA",
+  });
+
+  const res = await user.save();
+
+  console.log(res);
+};
+
+const addAddress = async (id) => {
+  const user = await User.findById(id);
+  user.addresses.push({
+    street: "99 3rd St.",
+    city: "New York",
+    state: "NY",
+    country: "USA",
+  });
+
+  const res = await user.save();
+
+  console.log("Add Address Res : ", res);
+};
+
+addAddress("64a35cb9750f65a21a78fc66");
